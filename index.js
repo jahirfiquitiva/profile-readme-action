977a3516f8db97243d7542f5685bc3e1a3b57ba1
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-11-16 20:35:44 -0500

    Better markdown

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const { Toolkit } = require('actions-toolkit');
 
-const { markdown} = require( "markdown" );
+const { markdown } = require('markdown');
 
 const getRecentActivity = require('./activity');
 const getFeed = require('./feed');
@@ -15,41 +15,41 @@ const MAX_FEED_LINES = core.getInput('MAX_FEED_LINES');
 const FEED_TO_HTML = core.getInput('FEED_TO_HTML');
 const FEED_URL = core.getInput('FEED_URL');
 
-const mdToHtml = (md) => (markdown.toHTML(md));
+const mdToHtml = (md) => markdown.toHTML(md);
 
 Toolkit.run(
   async (tools) => {
     try {
       tools.log.info('Getting user recent activity...');
-      const recentActivityLines = await getRecentActivity(
-        tools,
-        GH_USERNAME,
-        MAX_ACTIVITY_LINES
-      ).catch((error) => {
-        tools.log.error(error.message || 'Unexpected error getting recent activity!');
-        return [];
-      });
+      const recentActivityLines = await getRecentActivity(tools, GH_USERNAME, MAX_ACTIVITY_LINES)
+        .catch((error) => {
+          tools.log.error(error.message || 'Unexpected error getting recent activity!');
+          return [];
+        })
+        .join('\n');
       tools.log.success('Finished getting user recent activity:');
       console.log(recentActivityLines);
 
       if (ACTIVITY_TO_HTML) {
         tools.log.info('Parsing activity markdown to HTML...');
-        const htmlRecentActivity = recentActivityLines.map(mdToHtml);
-        console.log(htmlRecentActivity)
+        const htmlRecentActivity = mdToHtml(recentActivityLines);
+        console.log(htmlRecentActivity);
       }
 
       tools.log.info('Getting feed posts...');
-      const feedData = await getFeed(FEED_URL, MAX_FEED_LINES).catch((error) => {
-        tools.log.error(error.message || 'Unexpected error getting feed posts!');
-        return [];
-      });
+      const feedData = await getFeed(FEED_URL, MAX_FEED_LINES)
+        .catch((error) => {
+          tools.log.error(error.message || 'Unexpected error getting feed posts!');
+          return [];
+        })
+        .join('\n');
       tools.log.success('Finished getting user feed posts:');
       console.log(feedData);
 
       if (FEED_TO_HTML) {
         tools.log.info('Parsing feed posts markdown to HTML...');
-        const htmlFeedPosts = recentActivityLines.map(mdToHtml);
-        console.log(htmlFeedPosts)
+        const htmlFeedPosts = mdToHtml(recentActivityLines);
+        console.log(htmlFeedPosts);
       }
     } catch (error) {
       core.setFailed(error.message);
